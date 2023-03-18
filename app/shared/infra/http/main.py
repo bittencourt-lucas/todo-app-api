ee19f3a0b8a5c67b2f6dfa7a0d754bca84d322d7
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-03-18 09:29:50 -0300

    chore: another folder refactor and added SQLAlchemy to shared infra

--- app/shared/infra/http/main.py
+++ app/shared/infra/http/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.shared.http.routes.router import router
+from app.shared.infra.http.routes.router import router
 
 app = FastAPI()
 
