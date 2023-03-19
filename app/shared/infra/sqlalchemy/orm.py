e990d5c2a042404f99f25910d4583e688b57c0bb
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-03-19 15:18:31 -0300

    chore: missed one config needed for SQLite

--- app/shared/infra/sqlalchemy/orm.py
+++ app/shared/infra/sqlalchemy/orm.py
@@ -6,7 +6,10 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-engine = create_engine(os.environ.get('SQLALCHEMY_DATABASE_URL'))
+engine = create_engine(
+  os.environ.get('SQLALCHEMY_DATABASE_URL'),
+  connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
