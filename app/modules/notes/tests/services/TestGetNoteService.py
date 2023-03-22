04f7a9efa1481f49c0b35fbf9c27f462327ab390
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-03-22 06:51:18 -0300

    test: TestGetNoteService

---
+++ app/modules/notes/tests/services/TestGetNoteService.py
@@ -0,0 +1,22 @@
+import unittest
+from ...services.CreateNoteService import CreateNoteService
+from ...services.GetNoteService import GetNoteService
+from ...repositories.fakes.FakeNotesRepository import FakeNotesRepository
+
+class TestGetNoteService(unittest.IsolatedAsyncioTestCase):
+  def setUp(self):
+    notes_repository = FakeNotesRepository()
+    self.create_note = CreateNoteService(notes_repository)
+    self.get_note = GetNoteService(notes_repository)
+
+  async def test_execute(self):
+    title = 'Hello World'
+
+    note = await self.create_note.execute(title)
+    storedNote = await self.get_note(note.id)
+
+    self.assertEqual(note.id, storedNote.id)
+    self.assertEqual(note.title, storedNote.title)
+    self.assertEqual(note.completed, storedNote.completed)
+    self.assertEqual(note.order, storedNote.order)
+    
